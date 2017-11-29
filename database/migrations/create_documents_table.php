0235f294f4b69106013e54ed849866500dd19396
ferrada01 <ferrada.mardones@gmail.com>
2017-11-29 13:55:40 -0300

    Create create_documents_table.php

---
+++ database/migrations/create_documents_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDocumentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('documents', function (Blueprint $table) {
+            $table->increments('id')->unique();
+            $table->string('version');
+            $table->string('titulo');
+            $table->text('cuerpo');
+            $table->string('fecha');
+            $table->string('autor');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('documents');
+    }
+}
